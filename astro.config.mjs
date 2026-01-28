b2640a982892624a1085983585b80d9ed7d33a07
Eric Sexton <foopis23@gmail.com>
2026-01-28 11:51:56 -0500

    changed base content

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "REPLACE WITH YOUR GITHUB PAGES BASE URL (ie. https://<username>.github.io)",
-	base: "REPLACE WITH YOUR GITHUB REPOSITORY NAME (ie. /<repository-name>)",
+	base: "/repo-name", // replace 'repo-name' with your GitHub repository name
 });
