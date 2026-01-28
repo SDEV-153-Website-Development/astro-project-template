7f87b4a813ab04dedfda576572ae54ee555f0ed9
Eric Sexton <foopis23@gmail.com>
2026-01-28 10:40:25 -0500

    added github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "REPLACE WITH YOUR GITHUB PAGES BASE URL (ie. https://<username>.github.io)",
+	base: "REPLACE WITH YOUR GITHUB REPOSITORY NAME (ie. /<repository-name>)",
+});
